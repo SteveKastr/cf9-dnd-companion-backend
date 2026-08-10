4bcea42d8ef99c18b813e7dd08638a19889b42c8
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-08-10 20:06:51 +0300

    chore: add alphabetical sorting to Spells endpoint

--- src/main/java/gr/aueb/cf9/dndcompanion/controller/SpellController.java
+++ src/main/java/gr/aueb/cf9/dndcompanion/controller/SpellController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public class SpellController {
     public Page<Spell> getAllSpells(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
         return spellService.getAllSpells(pageable);
     }
 
